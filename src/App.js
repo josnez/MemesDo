e2016e3b8be58f0fbe7aed4920a726689ee90314
Josue Nuñez <josuenunezprada@gmail.com>
2021-09-17 11:12:25 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import html2canvas from 'html2canvas';
 
 function App() {
+
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  const onChangeLinea1 = function (evento) {
+    setLinea1(evento.target.value);
+  };
+
+  const onChangeLinea2 = function (evento) {
+    setLinea2(evento.target.value);
+  };
+
+  const onChangeImagen = function (evento) {
+    setImagen(evento.target.value);
+  };
+
+  const onClickExportar = function (evento) {
+    alert("exporrtar");
+    html2canvas(document.querySelector("#meme")).then(canvas => {
+      var img = canvas.toDataURL("image/png");
+
+      var link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = img;
+      link.click();
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={onChangeImagen}>
+        <option value="fire">casa en llamas</option>
+        <option value="history">history channel</option>
+        <option value="futurama">futurama</option>
+        <option value="matrix">matrix</option>
+      </select> <br />
+      <input onChange={onChangeLinea1} type="text" placeholder="Linea 1"></input><br />
+      <input onChange={onChangeLinea2} type="text" placeholder="Linea 2"></input><br />
+      <button onClick={onClickExportar}>Exportar</button>
+
+      <div className="meme" id="meme">
+        <span>{linea1}</span><br />
+        <span>{linea2}</span><br />
+        <img src={"/img/" + imagen + ".jpg"} />
+      </div>
     </div>
   );
 }
